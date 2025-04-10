dabee5a8e93ba2b57771be328f8d1b3e1e377894
gabriel <gabrielkr32@gmail.com>
2025-04-10 20:30:13 -0300

    Finalizando o porjeto

--- activity01.cpp
+++ activity01.cpp
@@ -15,7 +15,7 @@ int main() {
 
     cout << "Digite a pressão da cabine (Em Pascal): ";
     cin >> pressao;
-}
+
 
 //002 armazenamento  = agora vamos guardar os dados e crurar as variaveis adicionais parar trablhar
 
@@ -23,4 +23,26 @@ int temperaturaKelvin;  //guardar o valor convertido
 int pressaoAjustada;     // guardar um valor modificado pela variavel pressao
 bool sistemasDeSegurancaAtivo; // verifica se o ssitema foi ativo
 
+//003 processo de dados - operações aritimeticas com dados
+temperaturaKelvin = temperatura + 273; //celsis para kelvin
+pressaoAjustada = pressao * 2; //sumala a dobra de pressao por ajuste tecnico 
 
+
+///04 condições para o sistema de segurança
+if (temperatura > 100 && pressao < 5000) {
+    sistemasDeSegurancaAtivo = true;
+
+} else {
+    sistemasDeSegurancaAtivo = false;
+}
+
+//05 saida de dados - mosntar os resultados para o operador do sistema
+
+cout << "\n ====RELATORIO DO SISTEMA======\n";
+cout << "Temperatura convertida: " << temperaturaKelvin << "K\n";
+cout << "Pressão ajustada : " << pressaoAjustada << "Pascal\n";
+cout << "Sistema de segurança: " << (sistemasDeSegurancaAtivo ? "Ativado" : "Desativado") << endl;
+
+   return 0;
+
+}
